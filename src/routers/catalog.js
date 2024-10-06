2f0594e0849ab2422dab0bb35c74038d32b8a671
Yurii <143278195+Zharovskiy@users.noreply.github.com>
2024-10-06 09:46:26 +0300

    Update catalog.js

--- src/routers/catalog.js
+++ src/routers/catalog.js
@@ -4,6 +4,11 @@ import { getCatalogController } from '../controllers/catalog.js';
 
 const catalogRouter = Router();
 
-catalogRouter.get('/', ctrlWrapper(getCatalogController));
+catalogRouter.get('/', (req, res) => {
+  res.set('Cache-Control', 'no-store');
+  ctrlWrapper(getCatalogController);
+});
+
+// catalogRouter.get('/', ctrlWrapper(getCatalogController));
 
 export default catalogRouter;
